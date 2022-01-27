6f10bb20d845338c29695870ca186a4f136d60f3
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2022-01-27 16:11:38 +0530

    Add files via upload

---
+++ WWDC_20/KNN.playground/Sources/Helpers.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// Just a helper method
+public func consoleLineBreak(withHeading heading: String) {
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print(heading)
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+}
